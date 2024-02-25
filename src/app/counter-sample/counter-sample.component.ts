4c48ca25215e1157989788362ace1c642d23ee65
mehmetduran932 <mehmet.duran932@gmail.com>
2024-02-25 22:38:49 +0300

    angular unit test

---
+++ src/app/counter-sample/counter-sample.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-counter-sample',
+  templateUrl: './counter-sample.component.html',
+  styleUrls: ['./counter-sample.component.scss']
+})
+export class CounterSampleComponent implements OnInit {
+
+  value = 0;
+  message: string = ''
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  increment() {
+    if (this.value < 15) {
+      this.value += 1;
+      this.message = '';
+    } else {
+      this.message = 'Maximum reached!';
+    }
+  }
+
+  decrement() {
+    if (this.value > 0) {
+      this.value -= 1;
+      this.message = '';
+    } else {
+      this.message = 'Minimum reached!';
+    }
+  }
+
+}
